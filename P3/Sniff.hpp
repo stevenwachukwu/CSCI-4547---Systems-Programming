98ee4cbac320fd6fb3e08096b69025b28d37a429
Steve Nwachukwu <144286017+stevenwachukwu@users.noreply.github.com>
2025-03-19 00:23:15 -0400

    Create Sniff.hpp

---
+++ P3/Sniff.hpp
@@ -0,0 +1,24 @@
+//Written by Steve Nwachukwu
+
+#ifndef SNIFF_HPP
+#define SNIFF_HPP
+#include "Params.hpp"
+#include "FileID.hpp"
+#include <dirent.h>
+
+class Sniff {
+private:
+    Params params;
+    string dirName;
+    string dirPath;
+    vector <string> words;
+    vector <FileID> foundedFiles;
+    struct dirent* curEntry;
+
+public:
+    Sniff(const Params& p);
+    void oneDir();
+    FileID oneFile(const string& Fname, ino_t iNode_Val);
+};
+
+#endif
