866a790360f0cb9127b67d176d52d1951c9a6612
Steve Nwachukwu <144286017+stevenwachukwu@users.noreply.github.com>
2025-03-28 17:40:20 -0400

    Create Params.hpp

---
+++ P4/Params.hpp
@@ -0,0 +1,28 @@
+//Written by Steve Nwachukwu
+
+#ifndef PARAMS_HPP
+#define PARAMS_HPP
+#include <iostream>
+#include <fstream>
+#include <unistd.h>
+#include <string>
+#include <getopt.h>
+
+using namespace std;
+
+class Params {
+public:
+    //Data members of the Params class
+    string startDir = ".";
+    string outputFile;
+    bool verbose = false;
+    bool ignoreCase = false;
+    string searchWords;
+
+    //Function prototypes of the Params class
+    Params(int argc, char* argv[]);
+    void print() const;
+    static void usage ();
+};
+
+#endif
