392ff19c3f1a3a22af06cfaa8e2106a27771a37d
Steve Nwachukwu <144286017+stevenwachukwu@users.noreply.github.com>
2025-04-10 01:32:25 -0400

    Create enums.hpp

---
+++ P5/enums.hpp
@@ -0,0 +1,9 @@
+//Written by Steve Nwachukwu
+
+#ifndef ENUMS_HPP
+#define ENUMS_HPP
+
+enum Moods {lazy, prissy, overtired, greedy, cooperative};
+enum JobStatus {notStarted, working, complete};
+
+#endif //ENUMS_HPP
